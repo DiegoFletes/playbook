150cf9f943b79e079102d412ac1bc71902ef9b4f
DiegoFletes <diego.fletes@cbtis226.edu.mx>
2022-04-24 21:34:21 -0500

    proyecto 1-3

---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,5 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+const diego = new MissionCommander("Diego")
+const juan = new MissionCommander("Juan")
+console.log(carlo.name)
