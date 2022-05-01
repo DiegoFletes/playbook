b94a6a628633f4aa65d80e573821d57e229811ac
DiegoFletes <diego.fletes@cbtis226.edu.mx>
2022-05-01 18:26:39 -0500

    service1

--- weekly_mission_4/Refactoring/app.js
+++ weekly_mission_4/Refactoring/app.js
@@ -1,6 +1,11 @@
+const Reader = require("./lib/utils/Reader")
+const ExplorerService = require( "./lib/services/ExplorerService.js")
+const explorers = Reader.readJsonFile("./explorers.json")
+console.log(ExplorerService.filterByMission(explorers,"node"))
 
 
-
+/*
+const explorers = Reader.readJsonFile("explorers.json")
 
 // Part 2: Get the quantity of explorers names in node
 const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
@@ -59,4 +64,4 @@ const assignFizzBuzzTrick = function(explorer){
 const explorersInNodeAndFizzBuzzTrick = explorersInNode.map((explorer) => assignFizzBuzzTrick(explorer));
 
 // Part 8: Get a list of the explorers in node, if the score is divisible by 5 and 3, set the property trick and the value FIZZBUZZ, if is just divisible by 5 set the property trcik and the value BUZZ, if is just divisible by 3 set the property trick and the value FIZZ, otherwise set the property trick and the score value. TODO
-console.log(explorersInNodeAndBuzzTrick)
+console.log(explorersInNodeAndBuzzTrick)*/
--- weekly_mission_4/Refactoring/lib/services/ExplorerService.js
+++ weekly_mission_4/Refactoring/lib/services/ExplorerService.js
@@ -1,23 +1,14 @@
-export class ExplorerService {
-    constructor () {
-      this.filterByMission = filterByMission
-      this.getAmountOfExplorersByMission = getAmountOfExplorersByMission
-      this.getExplorersUsernamesByMission = getExplorersUsernamesByMission
-    }
-}
-
 class ExplorerService{
     static filterByMission(explorers, mission){
-        const explorerByMission = explorers.filter((explorer) => explorer.mission === mission)
-        return this.filterByMission
+        const explorerByMission = explorers.filter((explorer) => explorer.mission === mission, )
+        return explorerByMission
     }
     static getAmountOfExplorersByMission(explorers, mission){
-        //const explorerAmount = explorers.filter((explorer) => explorer.mission === mission)
-        return this.getAmountOfExplorersByMission
+      
     }
     static getExplorersUsernamesByMission(explorers, mission){
-
     }
+
 }
 
 
